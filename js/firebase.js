e8db2c7ffabad68186b949d462fbfe5b7e50db75
Alok shukla <118595563+ahaag4@users.noreply.github.com>
2025-12-15 11:07:27 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,13 +3,12 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCn9YSO4-ksWl6JBqIcEEuLx2EJN8jMj4M",
-  authDomain: "svms-c0232.firebaseapp.com",
-  databaseURL: "https://svms-c0232-default-rtdb.firebaseio.com",
-  projectId: "svms-c0232",
-  storageBucket: "svms-c0232.appspot.com",
-  messagingSenderId: "359201898609",
-  appId: "1:359201898609:web:893ef076207abb06471bd0"
+  apiKey: "AIzaSyDGpagCpskeOxzVO4C5qgdVUTeWO8rUZUU",
+  authDomain: "electrician-830b2.firebaseapp.com",
+  projectId: "electrician-830b2",
+  storageBucket: "electrician-830b2.firebasestorage.app",
+  messagingSenderId: "1053791458330",
+  appId: "1:1053791458330:web:fdbf5befa79aef2376eda3"
 };
 
 const app = initializeApp(firebaseConfig);
